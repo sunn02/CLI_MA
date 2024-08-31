c247c1b74f47249ba1607d5f715785287111fd7b
Sol <sollopez0002@hotmail.com>
2024-08-30 23:21:06 -0400

    Funciones agregadas e incompletas para la consulta y muestra de la informacion pedida junto con la creacion de parametros. Modificar su estructura si es necesario

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,44 @@
 const yargs = require("yargs"); //Importar el modulo yargs
-// --> http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 
 const clima = require()
 
 const options = yargs
     .usage(usage) //Metodo usage; se utiliza para personalizar el mensaje de ayuda que se muestra al usuario cuando escribe --help
-    .option("n", {alias:"Nombre de ciudad - pais", describe: "Introducir el nombre de la ciudad y su pais", type: "string", demandOption: false })
-    .help(true) //Habilita la opcion --help
-    .argv //yargs procesa los argumentos de la linea de comandos 
+    .option("ciudad", {alias:"Nombre de ciudad", describe: "Introduce el nombre de la ciudad", type: "string", demandOption: true })
+    .option("pais", {alias:"Nombre de pais", describe: "Introduce el nombre del pais", type: "string", demandOption: true })
+    .option("format", {alias:"Formato de archivo", describe: "Introduce el tipo de formato: JSON, CSV o texto plano", type: "string", demandOption: true })
 
+    .help(true) //Habilita la opcion --help
+    .argv; //yargs procesa los argumentos de la linea de comandos 
+
+
+const ciudad =  argv.ciudad;
+const pais =  argv.pais;
 
 function consultarAPI(ciudad,pais){
-    // key: 0cc3953448a2fd9596fd31ccf0933cc0
-    const appID= '0cc3953448a2fd9596fd31ccf0933cc0';
+    // key: 9cd3a9b4f8258188570da64348c66bfa
+    const appID= '9cd3a9b4f8258188570da64348c66bfa';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
-    // console.log(url);
-
-    
-    Spinner();
+    // console.log(url); --> Imprimir la URL para verificar
 
     fetch(url)
         .then( resultado =>  resultado.json())
         .then( datos => {
             
-            limpiarHTML();
-            
             if(datos.cod === '404'){
-                mostrarError('Cuidad no encontrada');
+                mostrarError('Ciudad no encontrada');
                 return
             }
-            // imprime html
+    
             mostrarClima(datos);
         });
+}
 
+// OJO!! AVERIGUAR EN COMO TOMAR LOS ARGUMENTOS DE CIUDAD Y PAIS, FUERA O DENTRO DE LA FUNCION
+
+
+function mostrarClima(datos){
+}
+
+function consultarFormato(){    
 }
